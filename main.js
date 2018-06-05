d1cc5c6b78476f7ab0ed2db3256ddf8ffff653fa
Tesla Lau <tesla9527@gmail.com>
2018-06-05 19:00:13 +0800

    Lesson4 - Event

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
+  	x: 0,
+  	y: 0,
     name: 'Timi',
+    age: 28,
     blog: 'https://tesla9527.github.io/',
     blogTag: '<a href="https://tesla9527.github.io/">Tesla Blog</a>'
   },
@@ -11,6 +14,16 @@ var app = new Vue({
   	},
   	getTime: function(){
   		return 'Current time is ' + new Date().toLocaleString()
+  	},
+  	add: function(inc){
+  		return this.age += inc
+  	},
+  	subtract: function(dec){
+  		return this.age -= dec
+  	},
+  	updateXY: function(event){
+  		this.x = event.offsetX;
+  		this.y = event.offsetY;
   	}
   }
 })
